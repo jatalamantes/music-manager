00c539e205c84759c9fde974839e21e837c5b4d2
jatalamantes <talamantesja@gmail.com>
2024-10-15 20:52:53 -0400

    Created PlaylistsController.cs

---
+++ Server/Controllers/PlaylistsController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using music_manager_start.Data.Models;
+using music_manager_starter.Data;
+using music_manager_starter.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace music_manager_starter.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaylistsController : ControllerBase
+    {
+        private readonly DataDbContext _context;
+
+        public PlaylistsController(DataDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        [HttpGet] //get playlist
+        public async Task<ActionResult<IEnumerable<Playlist>>> GetPlaylists()
+        {
+            return await _context.Playlists.ToListAsync();
+        }
+
+        
+        [HttpPost] //create playlist
+        public async Task<ActionResult<Playlist>> PostPlaylist(Playlist playlist)
+        {
+            if (playlist == null)
+            {
+                return BadRequest("Playlist cannot be null.");
+            }
+
+            playlist.Id = Guid.NewGuid();
+            _context.Playlists.Add(playlist);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetPlaylists), new { id = playlist.Id }, playlist);
+        }
+
+        
+        [HttpPut("{id}")]//changes to playlist
+        public async Task<IActionResult> PutPlaylist(Guid id, Playlist playlist)
+        {
+            if (id != playlist.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(playlist).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        
+        [HttpDelete("{id}")] //delete playlist
+        public async Task<IActionResult> DeletePlaylist(Guid id)
+        {
+            var playlist = await _context.Playlists.FindAsync(id);
+            if (playlist == null)
+            {
+                return NotFound();
+            }
+
+            _context.Playlists.Remove(playlist);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
