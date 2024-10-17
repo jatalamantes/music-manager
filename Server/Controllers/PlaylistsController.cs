fc774e605e183f27d3b6a0b963ec260d6e5d516e
jatalamantes <talamantesja@gmail.com>
2024-10-16 20:57:02 -0400

    worked on functionality of playlist page, added project reference from client to data for the playlist

---
+++ Server/Controllers/PlaylistsController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using music_manager_starter.Data;
+using music_manager_starter.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace music_manager_starter.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaylistsController : ControllerBase
+    {
+        private readonly DataDbContext _context;
+
+        public PlaylistsController(DataDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Playlist>>> GetPlaylists()
+        {
+            return await _context.Playlists.ToListAsync();
+        }
+
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Playlist>> GetPlaylist(Guid id)
+        {
+            var playlist = await _context.Playlists.FindAsync(id);
+            if (playlist == null)
+            {
+                return NotFound();
+            }
+            return playlist;
+        }
+
+        
+        [HttpPost]
+        public async Task<ActionResult<Playlist>> PostPlaylist(Playlist playlist)
+        {
+            if (playlist == null)
+            {
+                return BadRequest("Playlist cannot be null.");
+            }
+
+            _context.Playlists.Add(playlist);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetPlaylist), new { id = playlist.Id }, playlist);
+        }
+
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlaylist(Guid id, Playlist playlist)
+        {
+            if (id != playlist.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(playlist).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlaylistExists(id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePlaylist(Guid id)
+        {
+            var playlist = await _context.Playlists.FindAsync(id);
+            if (playlist == null)
+            {
+                return NotFound();
+            }
+
+            _context.Playlists.Remove(playlist);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PlaylistExists(Guid id)
+        {
+            return _context.Playlists.Any(e => e.Id == id);
+        }
+    }
+}
