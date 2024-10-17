0ced3aad557ae6669060324043cae946f079a2ac
jatalamantes <talamantesja@gmail.com>
2024-10-16 20:16:14 -0400

    Created Playlists.razor, and made connection to playlist button

--- music-manager-start.Data/Models/Playlist.cs
+++ music-manager-start.Data/Models/Playlist.cs
@@ -5,12 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using music_manager_starter.Data.Models;
 
-namespace music_manager_start.Data.Models
+namespace music_manager_starter.Data.Models
 {
     public class Playlist
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public List<Song> Songs { get; set; } = new List<Song>();
     }
 }
